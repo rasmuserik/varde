be21382fad0a7bc3a07c833409852e67a4096ac0
Jacob Andresen <jacob.andresen@gmail.com>
2019-05-04 19:59:35 +0200

    add notes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import './bookmarks'
 import IpfsHttpClient from 'ipfs-http-client'
 
 ;(function () {
+  // ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]
   let client = new IpfsHttpClient('localhost', 5001);
   document.getElementById('submit').onclick = function () {
     try {
-       client.dag.put({ data: "foo" });
-    } catch (err) {
-      console.log(err.toString());
+       //TODO: looks like dht needs a ipns adresss?
+       client.dht.put("foo", "bar", function (err) {
+         console.log("err :", err);
+
+         client.dht.get("foo", function (err2, value2) {
+           console.log("err2 :", err2)
+           console.log("value2:" , value2); 
+          });
+       });
+     } catch (err) {
+      console.log("OO NOEZ:", err.toString());
     }
   };
 
